1455e94a2048168079cefd37913536fd2af35f3e
shishirbohara <shishirbohara68@gmail.com>
2025-01-18 15:00:07 +0545

    feat: connect user registration in frontend

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 const studentRoutes = require("./routes/student");
 const userRoutes = require("./routes/userRoutes");
+const cors = require("cors");
 require("dotenv").config();
 
 app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 app.use("/", studentRoutes);
 app.use("/", userRoutes);
--- frontend/app/register/page.jsx
+++ frontend/app/register/page.jsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import Link from "next/link";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 export default function Registration() {
   const [formData, setFormData] = useState({
@@ -9,6 +10,13 @@ export default function Registration() {
     email: "",
     password: "",
   });
+  const [togglePassword, setTogglePassword] = useState(false);
+  const [error, setError] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
+
+  const togglePass = () => {
+    setTogglePassword(!togglePassword);
+  };
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -18,6 +26,33 @@ export default function Registration() {
     });
   };
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    setError("");
+    setSuccessMessage("");
+
+    try {
+      const response = await fetch("http://localhost:5000/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        setSuccessMessage(data.message);
+      } else {
+        setError(data.error);
+      }
+    } catch (error) {
+      setError("Error connecting to the server. Please try again.");
+    }
+  };
+
   return (
     <>
       <div className="flex items-center justify-center min-h-screen bg-gray-100">
@@ -25,7 +60,7 @@ export default function Registration() {
           <h1 className="text-2xl font-bold text-center text-gray-800 mb-6">
             User Registration
           </h1>
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className="mb-4">
               <label
                 htmlFor="username"
@@ -62,7 +97,7 @@ export default function Registration() {
               />
             </div>
 
-            <div className="mb-4">
+            <div className="mb-4 relative">
               <label
                 htmlFor="password"
                 className="block text-sm font-medium text-gray-700"
@@ -70,7 +105,7 @@ export default function Registration() {
                 Password
               </label>
               <input
-                type="password"
+                type={togglePassword ? "text" : "password"}
                 id="password"
                 name="password"
                 value={formData.password}
@@ -78,15 +113,31 @@ export default function Registration() {
                 required
                 className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm"
               />
+              <button
+                className="absolute top-9 right-5"
+                type="button"
+                onClick={togglePass}
+              >
+                {togglePassword ? <FaEye /> : <FaEyeSlash />}
+              </button>
             </div>
 
-            <button className="w-full py-2 px-4 bg-green-600 hover:bg-green-700 transition-colors text-white font-medium">
+            <button
+              type="submit"
+              className="w-full py-2 px-4 bg-green-600 hover:bg-green-700 transition-colors text-white font-medium"
+            >
               Register
             </button>
+
+            {successMessage && (
+              <div className="mt-4 text-green-600">{successMessage}</div>
+            )}
+            {error && <div className="mt-4 text-red-600">{error}</div>}
+
             <div className="flex justify-between mt-2">
               <p>Already have an account?</p>
               <Link href={"/login"} className="cursor-pointer underline">
-                Login here !
+                Login here!
               </Link>
             </div>
           </form>
